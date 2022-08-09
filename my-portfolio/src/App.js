5fbde04cc6bc68b7ae52090abbbe3f304793c299
fozz101 <fedi.galfat@esprit.tn>
2022-08-09 07:49:20 +0100

    About me section

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -43,6 +43,41 @@ function App() {
         <br />
         <br />
       </div>
+      <div className="about-me-section aboutme-inside my-5">
+        <div className="d-flex flex-column">
+          <span className="about-me-text"> About me</span>
+          <span className="why-text-sub"> Why Choose me ?</span>
+          <div className="row justify-content-center shadow-lg my-5">
+            <div className="col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center">
+              <lottie-player
+                src="https://assets6.lottiefiles.com/packages/lf20_v1yudlrx.json"
+                background="transparent"
+                speed="1"
+                style={{ width: "500px", height: "500px" }}
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 py-5 d-flex flex-column">
+              <span className="some-text-about-me">
+                Full Stack Web and mobile developer with background knowledge in
+                MERN Stack with Redux.
+              </span>
+              <span className="few-highlights">
+                <span className="few-text">Here are few highlights</span>
+                <span className="few-list">
+                  <ul>
+                    <li>Full Stack Web Developer </li>
+                    <li>Blockchain Enthusiast </li>
+                    <li>Java Developer </li>
+                    <li>Python ftw </li>
+                  </ul>
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
